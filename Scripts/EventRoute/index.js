8ec61c84278fd77ef04dbca66c214aef38147c19
Anthony Palmer <AnthonyP4312@gmail.com>
2021-01-19 22:32:02 -0500

    dist

--- Scripts/EventRoute/index.js
+++ Scripts/EventRoute/index.js
@@ -5,41 +5,42 @@ const Events_1 = require("../Events");
 exports.routePath = "/logEvent/";
 // Middleware for GET /events endpoint
 const eventsHandler = async (req, res, next) => {
-  // Mandatory headers and http status to keep connection open
-  const headers = {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  };
-  //OK!
-  res.writeHead(200, headers);
-  const startingEvents = Events_1.makeEvent({
-    body:
-      "Subscribing to CSPAM Times Real Time Ticker... All the Shit that's fit to Post...",
-  });
-  const stringData = JSON.stringify(startingEvents);
-  // After client opens connection send log events, bot settings
-  const data = `data: ${stringData}\n\n`;
-  res.write(data);
-  // Generate an id based on timestamp and save res
-  // object of client connection on clients list
-  // Later we'll iterate it and send updates to each client
-  const newClient = Events_1.makeNewClient(res);
-  //add the new client to the array of clients
-  Events_1.addClient(newClient);
-  // When client closes connection we update the clients list
-  // avoiding the disconnected one
-  req.on("close", () => {
-    console.log(`${newClient.id} Connection closed`);
-    Events_1.removeClient(newClient.id);
-  });
+    // Mandatory headers and http status to keep connection open
+    const headers = {
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+    };
+    //OK!
+    res.writeHead(200, headers);
+    const startingEvents = Events_1.makeEvent({
+        body: "Subscribing to CSPAM Times Real Time Ticker... All the Shit that's fit to Print...",
+    });
+    const stringData = JSON.stringify(startingEvents);
+    // After client opens connection send log events, bot settings
+    const data = `data: ${stringData}\n\n`;
+    res.write(data);
+    // Generate an id based on timestamp and save res
+    // object of client connection on clients list
+    // Later we'll iterate it and send updates to each client
+    const newClient = Events_1.makeNewClient(res);
+    //add the new client to the array of clients
+    Events_1.addClient(newClient);
+    // When client closes connection we update the clients list
+    // avoiding the disconnected one
+    req.on("close", () => {
+        console.log(`${newClient.id} Connection closed`);
+        Events_1.removeClient(newClient.id);
+    });
 };
-exports.eventRoute = async (req, res, next) => {
-  try {
-    eventsHandler(req, res, next);
-  } catch (error) {
-    res.status(500);
-    next(error);
-  }
+const eventRoute = async (req, res, next) => {
+    try {
+        eventsHandler(req, res, next);
+    }
+    catch (error) {
+        res.status(500);
+        next(error);
+    }
 };
+exports.eventRoute = eventRoute;
 //# sourceMappingURL=index.js.map
--- Scripts/Urls/index.js
+++ Scripts/Urls/index.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getThreadLastPostUrl = exports.getForumUrl = exports.getUserProfileUrl = exports.baseUrl = void 0;
 exports.baseUrl = "https://forums.somethingawful.com";
-exports.getUserProfileUrl = (profile) => `${exports.baseUrl}/member.php?action=getinfo&userid=${profile}`;
-exports.getForumUrl = (forumId) => `${exports.baseUrl}/forumdisplay.php?forumid=${forumId}`;
-exports.getThreadLastPostUrl = (threadId) => `${exports.baseUrl}/showthread.php?threadid=${threadId}&goto=lastpost`;
+const getUserProfileUrl = (profile) => `${exports.baseUrl}/member.php?action=getinfo&userid=${profile}`;
+exports.getUserProfileUrl = getUserProfileUrl;
+const getForumUrl = (forumId) => `${exports.baseUrl}/forumdisplay.php?forumid=${forumId}`;
+exports.getForumUrl = getForumUrl;
+const getThreadLastPostUrl = (threadId) => `${exports.baseUrl}/showthread.php?threadid=${threadId}&goto=lastpost`;
+exports.getThreadLastPostUrl = getThreadLastPostUrl;
 //# sourceMappingURL=index.js.map
